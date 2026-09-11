22cf7b13008c1e59ba6be08b345959ab1f27d836
eneskaanert <eneskaanert29@gmail.com>
2026-09-11 11:28:11 +0300

    cors vercel adresi eklendi

--- main.py
+++ main.py
@@ -25,6 +25,7 @@ app.add_middleware(
         "http://127.0.0.1:5174",
         "http://192.168.31.8:5173",
         "http://192.168.31.8:5174",
+         "https://citizen-app-lovat.vercel.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
